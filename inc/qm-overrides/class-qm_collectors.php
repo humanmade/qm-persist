92424e1a8b541cec4aeb89114ad1bcb4bc2790f7
Andrzej Misiewicz <andrzej@misiewicz.it>
2022-08-07 09:44:25 +0200

    Fix newline at end of file

--- inc/qm-overrides/class-qm_collectors.php
+++ inc/qm-overrides/class-qm_collectors.php
@@ -2,69 +2,69 @@
 
 if ( ! class_exists( 'QM_Collectors' ) ) {
 	
-class QM_Collectors implements IteratorAggregate {
-	private static $instance = null;
+	class QM_Collectors implements IteratorAggregate {
+		private static $instance = null;
 
-	private $items     = array();
-	private $processed = false;
+		private $items     = array();
+		private $processed = false;
 
-	public function getIterator() {
-		return new ArrayIterator( $this->items );
-	}
-
-	public static function add( QM_Collector $collector ) {
-		$collectors = self::init();
-
-		$collectors->items[ $collector->id ] = $collector;
-	}
-
-	public static function get( $id ) {
-		$collectors = self::init();
-		if ( isset( $collectors->items[ $id ] ) ) {
-			return $collectors->items[ $id ];
-		}
-		return false;
-	}
-
-	/* added/modified: */
-	public static function init() {
-		if ( ! static::$instance ) {
-			static::$instance = new QM_Collectors();
+		public function getIterator() {
+			return new ArrayIterator( $this->items );
 		}
 
-		return static::$instance;
+		public static function add( QM_Collector $collector ) {
+			$collectors = self::init();
 
-	}
-
-	public static function replace_instance( QM_Collectors $instance ) {
-		static::$instance = $instance;
-	}
-	/* end added/modified */
-
-	public function process() {
-		if ( $this->processed ) {
-			return;
+			$collectors->items[ $collector->id ] = $collector;
 		}
 
-		foreach ( $this as $collector ) {
-			$collector->tear_down();
-
-			$timer = new QM_Timer();
-			$timer->start();
-
-			$collector->process();
-			$collector->process_concerns();
-
-			$collector->set_timer( $timer->stop() );
+		public static function get( $id ) {
+			$collectors = self::init();
+			if ( isset( $collectors->items[ $id ] ) ) {
+				return $collectors->items[ $id ];
+			}
+			return false;
 		}
 
-		foreach ( $this as $collector ) {
-			$collector->post_process();
+		/* added/modified: */
+		public static function init() {
+			if ( ! static::$instance ) {
+				static::$instance = new QM_Collectors();
+			}
+
+			return static::$instance;
+
 		}
 
-		$this->processed = true;
+		public static function replace_instance( QM_Collectors $instance ) {
+			static::$instance = $instance;
+		}
+		/* end added/modified */
+
+		public function process() {
+			if ( $this->processed ) {
+				return;
+			}
+
+			foreach ( $this as $collector ) {
+				$collector->tear_down();
+
+				$timer = new QM_Timer();
+				$timer->start();
+
+				$collector->process();
+				$collector->process_concerns();
+
+				$collector->set_timer( $timer->stop() );
+			}
+
+			foreach ( $this as $collector ) {
+				$collector->post_process();
+			}
+
+			$this->processed = true;
+		}
+
 	}
-
-}
 
 }
